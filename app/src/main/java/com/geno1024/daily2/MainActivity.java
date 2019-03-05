47f8aea63936469e8b5951db55055b6ab5f1788f
Y. Z. Chen <754097987@qq.com>
2019-03-06 04:23:05 +0800

    Initial commit

---
+++ app/src/main/java/com/geno1024/daily2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.geno1024.daily2;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
