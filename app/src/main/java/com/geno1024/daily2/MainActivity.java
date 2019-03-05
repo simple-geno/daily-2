5b64123b5787e3d4e1109092239335d2cbe93e51
Y. Z. Chen <754097987@qq.com>
2019-03-06 04:37:12 +0800

    [Update] I am thinking the main structure.

--- app/src/main/java/com/geno1024/daily2/MainActivity.java
+++ app/src/main/java/com/geno1024/daily2/MainActivity.java
@@ -2,13 +2,43 @@ package com.geno1024.daily2;
 
 import android.app.*;
 import android.os.*;
+import android.content.*;
+import android.preference.*;
+import android.widget.*;
 
 public class MainActivity extends Activity 
 {
+    SharedPreferences sp;
+    SharedPreferences.Editor e;
+    
+    LinearLayout.LayoutParams mw;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        sp = PreferenceManager.getDefaultSharedPreferences(this);
+        e = sp.edit();
+        mw = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        int today = sp.getInt("progress", 0);
+        if (today == 0)
+            init();
+        update();
+    }
+    
+    public void init()
+    {
+        LinearLayout main = new LinearLayout(this);
+        main.setOrientation(LinearLayout.VERTICAL);
+        setContentView(main);
+        
+        TextView t = new TextView(this);
+        main.addView(t, mw);
+        
+    }
+    
+    public void update()
+    {
+
     }
 }
